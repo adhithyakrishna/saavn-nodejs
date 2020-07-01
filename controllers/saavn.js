9e4158a9c60edff82d4b4eff74fa39488d74c994
Adhithyakrishna <adhithya.coder@gmail.com>
2020-06-30 22:24:59 -0400

    Added routes to extract albums as well as song
    Should do error handling
    Should also do song fetch

--- controllers/saavn.js
+++ controllers/saavn.js
@@ -1,23 +1,27 @@
 const axios = require('axios');
 const { json } = require('body-parser');
 
-exports.getSearchResults = (req, res, next) => {
+exports.getAutoCompleteResults = (req, res, next) => {
 
 	axios.get('https://www.saavn.com/api.php?__call=autocomplete.get', {
 		params: {
 			_marker: 0,
-			query: req.params.id,
+			query: req.params.input,
 			ctx: "android",
 			_format: "json"
 		}
 	}).then(function (response) {
 		console.log(response.data);
 		res.send({ 'result': response.data });
+	}).catch(function (error) {
+		if (error.response) {
+			
+		}
 	});
 };
 
 
-exports.getSongResults = (req, res, next) => {
+exports.getSongSearchResults = (req, res, next) => {
 
 	let page = req.query.page === undefined ? 1 : req.query.page;
 	let count = req.query.count === undefined ? 10 : req.query.count;
@@ -25,12 +29,83 @@ exports.getSongResults = (req, res, next) => {
 	axios.get('https://www.saavn.com/api.php?__call=search.getResults', {
 		params: {
 			_marker: 0,
-			q: req.params.song,
+			q: req.params.input,
 			ctx: "android",
 			p: page,
 			n: count,
-			// api_version=4, add if required
+			api_version: 4,
 			_format: "json"
+		}
+	}).then(function (response) {
+		res.send({ 'result': response.data });
+	});
+};
+
+exports.getAlbumSearchResults = (req, res, next) => {
+
+	let page = req.query.page === undefined ? 1 : req.query.page;
+	let count = req.query.count === undefined ? 10 : req.query.count;
+
+	axios.get('https://www.saavn.com/api.php?__call=search.getAlbumResults', {
+		params: {
+			_marker: 0,
+			q: req.params.input,
+			ctx: "android",
+			p: page,
+			n: count,
+			api_version: 4,
+			_format: "json"
+		}
+	}).then(function (response) {
+		res.send({ 'result': response.data });
+	});
+};
+
+
+exports.getSongsFromAlbum = (req, res, next) => {
+
+	axios.get('https://www.saavn.com/api.php?__call=webapi.get', {
+		params: {
+			_marker: 0,
+			includeMetaTags: 0,
+			type: "album",
+			token: req.params.id,
+			ctx: "android",
+			api_version: 4,
+			_format: "json"
+		}
+	}).then(function (response) {
+		res.send({ 'result': response.data });
+	});
+};
+
+exports.getsongId = (req, res, next) => {
+
+	axios.get('https://www.saavn.com/api.php?__call=webapi.get', {
+		params: {
+			_marker: 0,
+			includeMetaTags: 0,
+			type: "song",
+			token: req.params.id,
+			ctx: "android",
+			api_version: 4,
+			_format: "json"
+		}
+	}).then(function (response) {
+		res.send({ 'result': response.data });
+	});
+};
+
+
+exports.getsong = (req, res, next) => {
+
+	axios.get('https://www.jiosaavn.com/api.php?__call=song.getDetails', {
+		params: {
+			cc:"in",
+			_marker: 0,
+			_format: "json",
+			model : "Redmi_5A",
+			pids: req.params.pid
 		}
 	}).then(function (response) {
 		res.send({ 'result': response.data });
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 const saavnController = require('../controllers/saavn');
 
-router.get('/search/song/:song', saavnController.getSongResults);
+router.get('/search/:input', saavnController.getAutoCompleteResults);
 
-router.get('/search/:id', saavnController.getSearchResults);
+router.get('/search/song/:input', saavnController.getSongSearchResults);
+
+router.get('/search/album/:input', saavnController.getAlbumSearchResults);
+
+router.get('/songs/album/:id', saavnController.getSongsFromAlbum);
+
+router.get('/song/:id', saavnController.getsongId);
+
+router.get('/music/:pid', saavnController.getsong);
 
 module.exports = router;
